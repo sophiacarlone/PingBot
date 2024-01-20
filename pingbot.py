b9529c4a39ab0e940fcf63f56084437bcd97b74c
sophiacarlone <sophiamcar@gmail.com>
2024-01-19 23:46:52 -0500

    working with google

--- pingbot.py
+++ pingbot.py
@@ -15,18 +15,19 @@ async def pinging():
     #TODO: all the servers
     hostname = "google.com" #example
     response = os.system("ping -c 1 " + hostname)
-
+    if response == 0:
+        print("still running")
     if response != 0:
         print(f"{hostname} is down!")
         warning = (f"{hostname} is down!")
         
-    for guild in client.guilds:
-        await guild.system_channel.send(warning)
+        for guild in client.guilds:
+            await guild.system_channel.send(warning)
  
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-    pinging()
+    pinging.start()
 
 f = open("token.txt", "r")
 token = f.readline().strip('\n')
