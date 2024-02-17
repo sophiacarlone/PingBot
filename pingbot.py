4e6ceedec016411b2c8bb94b5bae4db08ac00cbf
sophiacarlone <sophiamcar@gmail.com>
2024-02-16 21:35:24 -0500

    done. Maybe webhook

--- pingbot.py
+++ pingbot.py
@@ -6,37 +6,34 @@ import discord
 import os
 
 DNS_IP = "128.153.145.53"
-DNS = True
+MINUTES = 10 #change to how many minutes in between checks
+CHANNEL = 1033616359235538987 #change to channel preference
 
 #setting up discord client
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-@tasks.loop(minutes=10.0)  # 10 minutes
+
+#ping tests (aka the BEEF of the code)
+@tasks.loop(minutes = MINUTES)  
 async def pinging():
-    channel = client.get_channel(1033616359235538987)  # sys-admin channel         
+    channel = client.get_channel(CHANNEL)           
     
-    #try dns first
+    #try dns first so that you go straight to IPS
     response = os.system("ping -c 1 " + DNS_IP)
     if response != 0:
         warning = " DNS is down!"
         await channel.send(warning)
-        print("HITS")
-        DNS = False
+
         #if DNS is down, try the ip addresses
-     
-    if not dns:   
-        print("hello")
         try:
             with open("ip_server_name.txt", "r") as f:
                 for line in f:
                     hostname = line.strip('\n')
                     response = os.system("ping -c 1 " + hostname)
                         
-                    if response == 0:
-                        await channel.send(f"{hostname} ip worked.")
-                    else:
+                    if response != 0:
                         warning = (f"{hostname} is down!")
                         await channel.send(warning)
         
@@ -44,24 +41,10 @@ async def pinging():
             print("File 'ip_server_name.txt' not found.")
         except Exception as e:
             print(f"Error occurred while reading 'ip_server_name.txt': {e}")
-        
-        # with open("ip_server_name", "r") as f: #line with all ip addresses (could make a webhook later to automatically pull from zones)
-        #         print("hit c")
-        #         for line in f:
-        #             print("hit A")
-        #             hostname = line.strip('\n')
-        #             response = os.system("ping -c 1 " + hostname)
-                    
-        #             #could use a try catch, but nah
-        #             if response == 0:
-        #                 await channel.send(f"{hostname} ip worked.")
-                
-        #             else:
-        #                 warning = (f"{hostname} is down!")
-        #                 await channel.send(warning)
+
         f.close() 
         
-    #dns is up, so try those entries
+    #dns is up, so use domain names (more helpful for those who are unfamiliar which server has which ip address)
     else:
         try:                
             with open("dns_server_name.txt", "r") as f:
@@ -79,7 +62,9 @@ async def pinging():
             print(f"Error occurred while reading 'ip_server_name.txt': {e}")
         
         f.close()
-     
+    
+    
+#start of bot 
 @client.event
 async def on_ready():
     # print(f'We have logged in as {client.user}')
