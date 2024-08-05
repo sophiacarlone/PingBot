f153d58dad1df43f724e80cf085d129913dd4186
sophiacarlone <sophiamcar@gmail.com>
2024-08-05 16:13:50 -0400

    list and add done untested

--- commands.py
+++ commands.py
@@ -5,12 +5,58 @@ intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
+# List
 @tree.command(
-    name="commandname",
-    description="My first application Command"
+    name="list",
+    description="list addresses to be pinged"
 )
-async def first_command(interaction):
-    await interaction.response.send_message("Hello!")
+async def list(interaction):
+    serverList = ""
+    try:
+        with open("dns_server_name.txt", "r") as f:
+                for line in f:
+                   serverList += line                
+    except FileNotFoundError:
+            print("File 'dns_server_name.txt' not found.")
+    except Exception as e:
+            print(f"Error occurred while reading 'dns_server_name.txt': {e}")    
+    f.close()
+    try:
+        with open("ip_server_name.txt", "r") as f:
+                for line in f:
+                   serverList += line                
+    except FileNotFoundError:
+            print("File 'ip_server_name.txt' not found.")
+    except Exception as e:
+            print(f"Error occurred while reading 'ip_server_name.txt': {e}")    
+    f.close()
+    print(serverList)
+    await interaction.response.send_message(serverList)
+
+#Add    
+@tree.command(
+    name = "add",
+    description="add a server to pingbot"
+)
+async def first_command(interaction: discord.Interaction, message: str):
+    newServers = message.split("")
+    for l in newServers:
+        dnsFile = open("dns_server_name.txt", 'a')
+        IPFile = open("ip_server_name.txt", 'a')
+        if l.find(".clarkson.edu"):
+            dnsFile.write(l + "\n")
+        if l.find("128.153.14"):
+            IPFile.write(l + "\n")
+        dnsFile.close()
+        IPFile.close()
+    await interaction.response.send_message("added!")
+
+@tree.command(
+    name = "hold",
+    description = "hold on pinging all or one server"
+)
+async def second_command(interaction):
+    await interaction.response.send_message("Bye!")
     
 @client.event
 async def on_ready():
